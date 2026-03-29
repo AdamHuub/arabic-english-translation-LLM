6548b15aae4eea8d0cb452b717ae7353ea36aec1
AdamHub <adamdev667@outlook.com>
2026-03-29 02:40:34 +0100

    arabic  normalization

--- preprocessing/clean-arabic-text.py
+++ preprocessing/clean-arabic-text.py
@@ -1,13 +1,19 @@
 # import section
 
+import pyarabic.araby as araby
+import re
+def convert_ind_arabic_to_latin(text):
+    return text.translate(str.maketrans('٠١٢٣٤٥٦٧٨٩۰۱۲۳۴۵۶۷۸۹', '01234567890123456789'))
 
+         #text from user input 
+def Arabic_trans(text):
 
+    text = araby.normalize_alef(text) 
+    text = araby.strip_tatweel(text)
+    text = re.sub(r'([ء-ي])\1+', r'\1', text)
+    text = re.sub(r'[^\w\s\u0600-\u06FF]', '', text)
+    text = convert_ind_arabic_to_latin(text)
+    text = araby.tokenize(text)
+   
 
-def Arabic_trans():
-
-    with open("text.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-
-    print(text)
-
-
+return text
